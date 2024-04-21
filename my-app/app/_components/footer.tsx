36c8953dfc04a4be3c2c7fdf0b71aafc0e17ca43
Thakuei <c1247183@st.kanazawa-it.ac.jp>
2024-04-21 14:07:30 +0900

    footerにアイコンリンクを付与

--- my-app/app/_components/footer.tsx
+++ my-app/app/_components/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BxBxlGithub } from './icon/github-icon';
 import { LogosSlackIcon } from './icon/slack-icon';
+import { Fa6BrandsXTwitter } from './icon/x-icon';
 
 export default function Footer() {
     return (
@@ -14,11 +15,11 @@ export default function Footer() {
                 <p className="text-gray-500 dark:text-neutral-500">Under icons are my account link</p>
             </div>
             <div className="mt-5 space-x-8">
-                <a className="size-8 inline-flex justify-center items-center" href="#">
+                <a className="size-8 inline-flex justify-center items-center" href="https://github.com/Thakuei">
                     <BxBxlGithub className="flex-shrink-0 size-9" />
                 </a>
-                <a className="size-8 inline-flex justify-center items-center" href="#">
-                    <LogosSlackIcon className="flex-shrink-0 size-9" />
+                <a className="size-8 inline-flex justify-center items-center" href="https://twitter.com/Alpha606786241">
+                    <Fa6BrandsXTwitter className="flex-shrink-0 size-9" />
                 </a>
             </div>
         </div>
